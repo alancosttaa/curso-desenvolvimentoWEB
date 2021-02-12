78a50808cafde3e00c3148d2e1c8cbabe47b9709
alancosttaa <alan_marchetti@yahoo.com>
2021-02-11 22:11:56 -0300

    primeiro commit

---
+++ JS/fundamentos/organizacao.js
@@ -0,0 +1,20 @@
+//comentários
+/* bloco de comentário*/
+var a = 30;
+let b = 50;
+
+console.log(a,b);
+
+a = 10;
+b = 20;
+
+console.log(a,b);
+
+var a = 100;
+ b = 200; // se colocar let b = valor irá dar erro, so posso declarar se for o "var"
+
+console.log(b,a);
+
+const c = 500;
+
+console.log(a,b,c);
